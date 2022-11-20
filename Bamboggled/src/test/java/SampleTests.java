e0b902ffa5e15913e2260429b9e14f78badf78a9
Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>
2022-11-19 21:08:30 -0500

    Create sample tests
    For testing CircleCi and TravisCi

---
+++ Bamboggled/src/test/java/SampleTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTests {
+    @Test
+    public void sampleTest() {
+        assertEquals(1 + 1, 2);
+    }
+}
