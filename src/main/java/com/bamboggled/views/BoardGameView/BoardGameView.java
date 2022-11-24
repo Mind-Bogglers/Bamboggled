700c866e93b3e9d48dd7662c985c41888f464e80
mzafar2391 <116319521+mzafar2391@users.noreply.github.com>
2022-11-23 21:34:32 -0500

    [DEV-1.3] Grid label access and bg colour change
    Tested out methods to traverse over the grid and change different aspects of labels

--- src/main/java/com/bamboggled/views/BoardGameView/BoardGameView.java
+++ src/main/java/com/bamboggled/views/BoardGameView/BoardGameView.java
@@ -1,4 +1,31 @@
 package com.bamboggled.views.BoardGameView;
 
+//import javafx.*;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import static javafx.scene.layout.GridPane.getRowIndex;
+
 public class BoardGameView {
+
+    Node[][] labels = new Node[4][4];
+
+    @FXML GridPane grid4;
+
+
+    public void test() {
+        for (Node n: grid4.getChildren()) {
+            Label newNode = (Label) n;
+            System.out.println(newNode.getText());
+            System.out.println(newNode.getBackground().getFills());
+        }
+    }
+
+
+
+
+
+
 }
