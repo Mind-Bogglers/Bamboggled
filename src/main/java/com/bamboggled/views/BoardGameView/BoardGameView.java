c3fb7988470ea5b18f2877d01c59238ec1153cd8
mzafar2391 <116319521+mzafar2391@users.noreply.github.com>
2022-11-23 23:15:45 -0500

    [DEV-1.3] Created the method for initArray
    Co-Authored-By: Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>

--- src/main/java/com/bamboggled/views/BoardGameView/BoardGameView.java
+++ src/main/java/com/bamboggled/views/BoardGameView/BoardGameView.java
@@ -6,13 +6,27 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
-import static javafx.scene.layout.GridPane.getRowIndex;
 
 public class BoardGameView {
 
-    Node[][] labels = new Node[4][4];
+    Label[][] labels = new Label[4][4];
 
     @FXML GridPane grid4;
+
+    public void initLabelArray(){
+        for (int i = 0; i<4; i++){
+            for (int j = 0; j<4; j++){
+                for (Node n: grid4.getChildren()){
+                    if (n instanceof Label){
+                        if (GridPane.getRowIndex(n) == i && GridPane.getColumnIndex(n) == j){
+                            labels[i][j] = (Label) n;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
 
 
     public void test() {
