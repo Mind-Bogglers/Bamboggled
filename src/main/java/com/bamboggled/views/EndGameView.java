5816f003d180eee2afba52f3e92ec249104adf06
Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>
2022-11-26 12:33:35 -0500

    [DEV-1.3] Complete GameEndView

--- src/main/java/com/bamboggled/views/EndGameView.java
+++ src/main/java/com/bamboggled/views/EndGameView.java
@@ -1,11 +1,15 @@
 package com.bamboggled.views;
 
 import com.bamboggled.model.model.BoggleModel;
+import com.bamboggled.model.player.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -18,7 +22,8 @@ public class EndGameView {
     Stage stage;
 
     @FXML
-    Button button;
+    Button playAgainButton;
+    Label winnerLabel;
 
     public EndGameView(Stage stage){
         this.model = BoggleModel.getInstance();
@@ -32,6 +37,25 @@ public class EndGameView {
         try {
             AnchorPane root = loader.load();
             Scene scene = new Scene(root);
+            winnerLabel = (Label) root.lookup("#winnerLabel");
+            playAgainButton = (Button) root.lookup("#playAgainButton");
+            playAgainButton.setOnAction(this::restartgame);
+            playAgainButton.setText("Play Again");
+
+            //get the player who won
+            String winner = "Nobody";
+            int maxScore = -1;
+            for (Player p: model.getPlayers()) {
+                if (p.getScore() > maxScore) {
+                    maxScore = p.getScore();
+                    winner = p.getName();
+                }
+            }
+            //set the label to the winner
+            winnerLabel.setText(winner + " won!");
+
+
+
             this.stage.setScene(scene);
             stage.show();
 
@@ -47,7 +71,7 @@ public class EndGameView {
 
     public void restartgame(ActionEvent e){
         try {
-            new PlayView(this.stage);
+            new PlayView((Stage) ((Node) e.getSource()).getScene().getWindow());
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
