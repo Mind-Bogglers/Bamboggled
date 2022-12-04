2d4e8bf371f37c5384b7fcfca5c64ae641454a5b
Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>
2022-12-03 19:17:28 -0500

    [DEV-2.1] Update FXML load strategy for welcomeview and implement blind mode

--- src/main/java/com/bamboggled/views/WelcomeView.java
+++ src/main/java/com/bamboggled/views/WelcomeView.java
@@ -2,6 +2,7 @@ package com.bamboggled.views;
 
 import com.bamboggled.model.model.BoggleModel;
 
+import com.bamboggled.screenreader.ScreenReader;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 import javafx.event.ActionEvent;
@@ -9,52 +10,85 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class WelcomeView {
+    private final Stage stage;
+    private final ScreenReader screenReader;
+    private boolean visImpaired;
+    private Button playButton;
+    private Button instructionsButton;
 
-    private BoggleModel model;
 
-    private Stage stage;
-
-
-    public WelcomeView(Stage stage) throws IOException {
-        this.model = BoggleModel.getInstance();
+    public WelcomeView(Stage stage, boolean visImpaired) throws IOException {
+        this.screenReader = new ScreenReader();
+        this.visImpaired = visImpaired;
         this.stage = stage;
         start();
     }
 
-    public WelcomeView() {
-        this.model = BoggleModel.getInstance();
-    }
-
 
     private void start() throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/WelcomeView.fxml")));
-        this.stage.setTitle("Bamboogled");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/WelcomeView.fxml"));
+        loader.setController(this);
+        Parent root = loader.load();
+        this.playButton = (Button) root.lookup("#playBtn");
+        this.instructionsButton = (Button) root.lookup("#rulesBtn");
+        this.playButton.setOnAction(this::play);
+        this.instructionsButton.setOnAction(this::rules);
+        root.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+                System.exit(0);
+            } else if (event.isControlDown() && event.getCode() == KeyCode.B) {
+                this.visImpaired = !this.visImpaired;
+                if (this.visImpaired) {
+//                    this.screenReader.speak("Welcome to Bamboggled.  Press Control B after this message is complete to disable Visually Impaired Mode. Press I for instructions. Press P to play. Press Escape to exit.");
+                    this.screenReader.speak("Test");
+                } else {
+                    ScreenReader.voice.getAudioPlayer().cancel();
+                }
+            } else if (event.getText().equals("p") || event.getCharacter().equals("P")) {
+                System.out.println("HERE");
+                playButton.fire();
+            } else if (event.getText().equals("i") || event.getCharacter().equals("I")) {
+                instructionsButton.fire();
+            }
+        });
+        this.stage.setTitle("Bamboggled");
         this.stage.setScene(new Scene(root));
         this.stage.show();
 
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        if (this.visImpaired) {
 
-        Voice voice = VoiceManager.getInstance().getVoice("kevin16");
-        voice.allocate();
-        setVoice(voice);
-        //voice.speak("Welcome to Bamboggled.  Press Control B to disable Visually Impaired Mode at any time.");
-
+            //        this.screenReader.speak("Welcome to Bamboggled.  Press Control B after this message is complete to disable Visually Impaired Mode. Press I for instructions. Press P to play. Press Escape to exit.");
+            this.screenReader.speak("Test");
+        }
     }
 
 
-    public void play(ActionEvent e) throws IOException {
-        new PlayView((Stage) ((Node) e.getSource()).getScene().getWindow());
+    public void play(ActionEvent e) {
+        try {
+            new PlayView((Stage) ((Node) e.getSource()).getScene().getWindow(), this.visImpaired);
+        } catch (IOException ioException) {
+            System.out.println("Error loading play view");
+        }
 //        stage.close();
     }
 
-    public void rules(ActionEvent e) throws IOException {
-        new InstructionsView((Stage) ((Node) e.getSource()).getScene().getWindow());
+    public void rules(ActionEvent e) {
+        try {
+            new InstructionsView((Stage) ((Node) e.getSource()).getScene().getWindow(), this.visImpaired);
+        } catch (IOException ioException) {
+            System.out.println("Error loading welcome view");
+        }
     }
 
     private void setVoice(Voice voice) {
