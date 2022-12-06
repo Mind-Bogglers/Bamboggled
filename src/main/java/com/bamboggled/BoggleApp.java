fbec52e2e9ecde0ac13e8c4d163f87c5c707ff8e
Kevin Thevara <kevinthevara@gmail.com>
2022-12-05 23:50:40 -0500

    [DEV-3.2] Documented BoggleApp

--- src/main/java/com/bamboggled/BoggleApp.java
+++ src/main/java/com/bamboggled/BoggleApp.java
@@ -8,16 +8,33 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Application for running the Bamboggled game.
+ *
+ * Created by Hassan El-Sheika, Mustafa Zafar, Sultan Al-Dhalaan, and Kevin Thevara
+ * */
+
 public class BoggleApp extends Application {
 
-    BoggleModel model;
-
+    /**
+     * Attribute that refers to the view for welcome screen
+     * */
     WelcomeView welcomeView;
 
+    /**
+     * Main method
+     * @param args Launches the application
+     * */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Starts the JavaFX application
+     *
+     * @param beginStage Stage object that will contain the application
+     * @throws IOException Exception thrown if WelcomeView could not be loaded
+     * */
     @Override
     public void start(Stage beginStage) throws IOException {
         this.welcomeView = new WelcomeView(beginStage, true);
